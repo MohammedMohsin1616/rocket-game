95a6a2899b63a22759e684cb16bc55ade7f501ff
MohammedMohsin1616 <mohammedmohsinsayed@Mohammeds-MacBook-Air.local>
2018-10-22 18:05:14 +0200

    first commit

--- hustle-mode/ViewController.swift
+++ hustle-mode/ViewController.swift
@@ -7,12 +7,34 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
 
+    
+    @IBOutlet weak var darkBlueBackground: UIImageView!
+    @IBOutlet weak var cloudHolder: UIView!
+    @IBOutlet weak var rocket: UIImageView!
+    @IBOutlet weak var hustleLabel: UILabel!
+    @IBOutlet weak var onLabel: UILabel!
+    @IBOutlet weak var powerButton: UIButton!
+    
+    var player: AVAudioPlayer!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        let path = Bundle.main.path(forResource: "hustle-on", ofType: "wav")!
+        let url = URL(fileURLWithPath: path)
+        
+        do {
+            player = try AVAudioPlayer(contentsOf: url)
+            player.prepareToPlay()
+            
+        }
+        catch let error as NSError {
+            print(error.localizedDescription)
+        }
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +42,23 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
+    @IBAction func powerButtonPressed(_ sender: Any) {
+        
+        cloudHolder.isHidden = false
+        darkBlueBackground.isHidden = true
+        powerButton.isHidden = true
+        
+        player.play()
+        
+        UIView.animate(withDuration: 2.3, animations: {
+            
+            self.rocket.frame = CGRect(x: 0, y: 140, width: 375, height: 402)
+            
+        }) { (finished) in
+            self.hustleLabel.isHidden = false
+            self.onLabel.isHidden = false
+        }
+    }
+    
 }
 
